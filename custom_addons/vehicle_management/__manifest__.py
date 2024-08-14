8608255eac172dd9cb53def0f9b34d08e7dbb3e6
Adithya <adithyaak1612@gmail.com>
2024-08-14 20:34:14 +0530

    [ADD]Initial commit

--- custom_addons/vehicle_management/__manifest__.py
+++ custom_addons/vehicle_management/__manifest__.py
@@ -1,22 +1,17 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': "Vehicle Management",
     'description': "Vehicle management",
-    'application': True,
+    'application':True,
 
-    'depends': [
+    'depends':[
          'base',
          'fleet',
-         'hr',
-         'mail',
-
+         'mail'
     ],
 
     'data': [
         'views/vehicle_management_views.xml',
         'views/repair_tag_views.xml',
-        'data/load_data.xml',
-        'security/ir.model.access.csv',
+        'security/ir.models.access.csv',
     ]
 }
--- custom_addons/vehicle_management/models/__init__.py
+++ custom_addons/vehicle_management/models/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import vehicle_management
 from . import repair_tag
