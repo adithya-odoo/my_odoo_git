720028857e9ded4b2161196a0995c55da92a9229
adithya-odoo <adithyaak1612@gmail.com>
2024-08-30 20:17:54 +0530

    Delete custom_addons directory

--- custom_addons/associated_product/models/sale_order_line.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-class SaleOrderLine(models.Model):
-    """ To set a new field inside the 'sale.order.line' """
-    _inherit = 'sale.order.line'
-
-    is_line_associated_product = fields.Boolean(readonly=True, default=False)
