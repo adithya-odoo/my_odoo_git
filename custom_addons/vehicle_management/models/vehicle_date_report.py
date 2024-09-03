b57fc1a0eac6c2d1467d00b1179f71195544b710
Adithya <adithyaak1612@gmail.com>
2024-09-03 21:20:51 +0530

    sept 3

--- custom_addons/vehicle_management/models/vehicle_date_report.py
+++ custom_addons/vehicle_management/models/vehicle_date_report.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api
+from odoo import api, models
 
 
 class VehicleDateReport(models.AbstractModel):
@@ -8,17 +8,39 @@ class VehicleDateReport(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, doc_ids, data=None):
-        query = """ select name from vehicle_management where start_date between '%s' and '%s'"""% (data.get('start_date'),(data.get('end_date')))
-        # query += """ and advisor_id = %d """% (data.get('advisor_ids'))
+        query = """ select model.name as vehicle_name, partner.name as customer_name,
+                     users.name as advisor, vm.vehicle_number as vehicle_number, cat.name as cat,
+					 upper(vm.state) as state, upper(vm.service_type) as service_type , vm.total_cost as cost,
+					 vm.estimated_amount as estimated_amount from vehicle_management as vm
+                     inner join res_partner as partner on partner.id = vm.partner_id
+                     inner join fleet_vehicle_model as model on  model.id = vm.vehicle_id
+				     inner join fleet_vehicle_model_category as cat on cat.id = vm.vehicle_type_id
+					 inner join res_partner as users on users.id =  (select partner_id from res_users where id = advisor_id)"""
+
+        if data.get('start_date') and data.get('end_date'):
+            query += """ where start_date between '%s' and '%s' """% (data.get('start_date'), (data.get('end_date')))
+        if data.get('customer_ids'):
+            query += """ and partner_id in %s """% (str(tuple(data.get('customer_ids')))).replace(",)",")")
+        if data.get('advisor_ids'):
+            query += """ and advisor_id in %s """% (str(tuple(data.get('advisor_ids')))).replace(",)",")")
+
         print(query)
+
         self.env.cr.execute(query)
         report = self.env.cr.dictfetchall()
         print(report)
-        # docs = self.env['vehicle.management'].browse(doc_ids)
+        print(data.get('advisor_len'))
+        print(data.get('customer_len'))
+
+        if data.get('advisor_ids'):
+            for record in report:
+                record['advisor_len'] = data.get('advisor_len')
+        if data.get('customer_ids'):
+            for record in report:
+                record['customer_len'] = data.get('customer_len')
+        print(report)
+        docs = self.env['vehicle.management'].browse(doc_ids)
         return {
-            # 'doc_ids': doc_ids,
             'report': report,
             'doc_model': 'date.filter',
-            # 'docs': docs,
-            # 'data': data,
         }
--- custom_addons/vehicle_management/wizard/date_filter.py
+++ custom_addons/vehicle_management/wizard/date_filter.py
@@ -2,6 +2,8 @@
 
 from odoo import fields, models
 
+from odoo.exceptions import ValidationError
+
 
 class DateFilter(models.TransientModel):
    """This model is used to pop up a report filter."""
@@ -14,16 +16,29 @@ class DateFilter(models.TransientModel):
    advisor_ids = fields.Many2many('res.users', string="Advisor")
 
    def action_print_date_pdf(self):
-      # vehicle_management = self.env['vehicle.management'].search([('is_active', '=', True)])
-      print(self.start_date)
-      print(self.end_date)
+      if self.start_date and self.end_date:
+         if self.start_date > self.end_date:
+            raise ValidationError('End date should be larger than start date')
 
       data = {
-         'end_date': self.end_date,
-         'start_date': self.start_date,
-         'advisor_ids': self.advisor_ids,
-         'customer_ids':self.customer_ids,
-      }
+            'end_date': self.end_date,
+            'start_date': self.start_date,
+         }
+      for rec in self.customer_ids:
+         data.setdefault('customer_ids', [])
+         data['customer_ids'].append(rec.id)
+
+      for rec in self.advisor_ids:
+         data.setdefault('advisor_ids', [])
+         data['advisor_ids'].append(rec.id)
+
+      if self.advisor_ids:
+         data['advisor_len'] = len(data['advisor_ids'])
+      if self.customer_ids:
+         data['customer_len'] = len(data['customer_ids'])
+
+      print(data)
 
       return (self.env.ref(
          'vehicle_management.action_report_vehicle_management').report_action(None, data=data))
+
